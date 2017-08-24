d698fc344dc0a20c4da4d9cc9d3a5b9acaf700e7
epoiseau <eric.poiseau@inria.fr>
2017-08-24 12:17:28 +0200

    Fork of the project from google code

---
+++ src/main/java/com/googlecode/crowdin/maven/ExportCrowdinMojo.java
@@ -0,0 +1,44 @@
+package com.googlecode.crowdin.maven;
+
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Export crowdin translations in this project, for a fresh translation file
+ * 
+ * @goal export
+ * @aggregator
+ * @threadSafe
+ */
+public class ExportCrowdinMojo extends AbstractCrowdinMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		super.execute();
+
+		getLog().info("Asking crowdin to export translations");
+
+		String uri = "https://api.crowdin.com/api/project/" + authenticationInfo.getUserName() + "/export?key="
+				+ authenticationInfo.getPassword();
+
+		getLog().info(
+				"Calling " + "https://api.crowdin.com/api/project/" + authenticationInfo.getUserName() + "/export?key="
+						+ "?????");
+		GetMethod getMethod = new GetMethod(uri);
+		int returnCode;
+		try {
+			returnCode = client.executeMethod(getMethod);
+		} catch (Exception e) {
+			throw new MojoExecutionException("Failed to call API", e);
+		}
+		getLog().debug("Return code : " + returnCode);
+		if (returnCode != 200) {
+			throw new MojoExecutionException("Failed to export translations from crowdin");
+		}
+
+		// Post doesn't work ?
+		// crowdinRequestAPI("export", null, null, true);
+
+	}
+}
